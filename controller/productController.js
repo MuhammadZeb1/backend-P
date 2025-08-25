3a01167722f07e891d0b6197a8e73877e526a4a6
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-08-25 01:35:15 -0700

    make the red the read single product

--- controller/productController.js
+++ controller/productController.js
@@ -41,3 +41,21 @@ export const readProducts = async(req,res)=>{
     }
 }
 
+export const  readSingleProduct = async (req,res)=>{
+
+    try {
+        const {id}= req.params;
+        const product = await Product.findById(id).populate("vendor","name email role");
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+          }
+            res.status(200).json({ product });
+    } catch (error) {
+        
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+
+}
+    
+
+

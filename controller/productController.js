2cc3e6eec2bdf28d409acfb6e5e1adaef9394e05
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-08-25 02:22:23 -0700

    make the red the update product product

--- controller/productController.js
+++ controller/productController.js
@@ -51,11 +51,39 @@ export const  readSingleProduct = async (req,res)=>{
           }
             res.status(200).json({ product });
     } catch (error) {
-        
+
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 
 }
     
+export const updateProduct = async (req,res)=>{
+    try {
+        const {id}=req.params;
+        const {productName,title,price,category} = req.body;
+        const image = req.file.filename;
+
+        const updatedData = {};
+        if (productName) updatedData.productName = productName;
+        if (title) updatedData.title = title;
+        if (price) updatedData.price = price;
+        if (category) updatedData.category = category;
+        if (image) updatedData.image = image;
+
+        const product = await Product.findById(id);
+        if (!product) {
+            return res.status(404).json({ message: "Product not found" });
+        }
+        if (product.vendor.toString() !== req.user.id) {
+            return res.status(403).json({ message: "You are not authorized to update this product" });
+        }
+
+        const updateProduct = await Product.findByIdAndUpdate(id,{$set:updatedData},{new:true});
+        res.status(200).json({message:"Product updated successfully",product:updateProduct})
+        
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+}
 
 

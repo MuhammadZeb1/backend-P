b19b1dcea35296fa41445a3ab0df5c253c133ce4
MuhammadZeb1 <zebafridiuu@gmail.com>
2025-09-02 23:43:24 -0700

    add the deshboard api get the vendor and then get single vendor products

--- controller/vendorController.js
+++ controller/vendorController.js
@@ -1,5 +1,50 @@
+import Vendors from '../model/roleBasedAuthModel.js'
+import Product from "../model/productModel.js"
 
-
-export const vendorDeshborad = (req,res)=>{
-    res.send("hello")
+export const vendorDeshborad = async(req,res)=>{
+    try {
+        const vendors = await Vendors.find({role:"vendor"})        
+    if (vendors.length === 0) {
+      return res
+        .status(404)
+        .json({ status: false, message: "No vendors found" });
+    }
+            res.status(200).json(vendors)
+    } catch (error) {
+         res.status(500).json({ message: error.message });
+    }
 }
+export const singleVendorProducts = async (req, res) => {
+  try {
+    const { id } = req.params; // vendor id
+
+    // پہلے vendor نکالیں
+    const vendor = await Vendors.findById(id).select("name shopName shopType");
+
+    if (!vendor || vendor.role !== "vendor") {
+      return res
+        .status(404)
+        .json({ status: false, message: "Vendor not found" });
+    }
+
+    // پھر اس vendor کے products نکالیں
+    const products = await Product.find({ vendor: id });
+
+    if (!products.length) {
+      return res
+        .status(404)
+        .json({ status: false, message: "No products found for this vendor" });
+    }
+
+    res.status(200).json({
+      status: true,
+      vendor,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({ status: false, message: error.message });
+  }
+};
+
+
+
